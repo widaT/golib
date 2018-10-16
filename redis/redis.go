0939116b653ad85aa63bc1515c6296b2acd818bd
wida <wida@foxmail.com>
2018-10-16 13:37:33 +0800

    添加redis package

--- redis/redis.go
+++ redis/redis.go
@@ -37,20 +37,8 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
-var (
-	DefaultKey = "WebserverRedis"
-	Cache = redisCache{}
-)
-
-/*
-func init()  {
-	Cache.StartAndGC(fmt.Sprintf(`{"conn": "%s"}`,config.RedisConn))
-}
-*/
-
-
-type redisCache struct {
-	p        *redis.Pool // redis connection pool
+type Redis struct {
+	p        *redis.Pool
 	conninfo string
 	dbNum    int
 	key      string
@@ -58,7 +46,7 @@ type redisCache struct {
 }
 
 
-func (rc *redisCache) do(commandName string, args ...interface{}) (reply interface{}, err error) {
+func (rc *Redis) do(commandName string, args ...interface{}) (reply interface{}, err error) {
 	c := rc.p.Get()
 	defer c.Close()
 
@@ -66,21 +54,21 @@ func (rc *redisCache) do(commandName string, args ...interface{}) (reply interfa
 }
 
 //SISMEMBER key member
-func (rc *redisCache)Sismember(key string,member interface{}) (int,error) {
+func (rc *Redis)Sismember(key string,member interface{}) (int,error) {
 	return redis.Int(rc.do("SISMEMBER",key,member))
 }
 
 
-func (rc *redisCache)Smembers(key string) ([]string,error) {
+func (rc *Redis)Smembers(key string) ([]string,error) {
 	return redis.Strings(rc.do("SMEMBERS",key))
 }
 
-func (rc *redisCache)Scard(key string) (int,error) {
+func (rc *Redis)Scard(key string) (int,error) {
 	return redis.Int(rc.do("SCARD",key))
 }
 
 
-func (rc *redisCache)Srem(key string,member interface{}) error {
+func (rc *Redis)Srem(key string,member interface{}) error {
 	var err error
 	if _, err = rc.do("Srem", key,member); err != nil {
 		return err
@@ -88,7 +76,7 @@ func (rc *redisCache)Srem(key string,member interface{}) error {
 	return err
 }
 
-func (rc *redisCache) Sadd (key string,member interface{})  error {
+func (rc *Redis) Sadd (key string,member interface{})  error {
 	var err error
 	if _, err = rc.do("SADD", key,  member); err != nil {
 		return err
@@ -96,33 +84,33 @@ func (rc *redisCache) Sadd (key string,member interface{})  error {
 	return nil
 }
 
-func (rc *redisCache)Zrange(key string,start,stop int,withscores bool) ([]string,error){
+func (rc *Redis)Zrange(key string,start,stop int,withscores bool) ([]string,error){
 	if withscores {
 		return redis.Strings(rc.do("ZRANGE", key, start, stop,"WITHSCORES"))
 	}
 	return redis.Strings(rc.do("ZRANGE", key, start, stop))
 }
 
-func (rc *redisCache)Zrem(key ,member string) (int, error)  {
+func (rc *Redis)Zrem(key ,member string) (int, error)  {
 	return redis.Int(rc.do("ZREM", key, member))
 }
 
-func (rc *redisCache) Get(key string) interface{} {
+func (rc *Redis) Get(key string) interface{} {
 	if v, err := rc.do("GET", key); err == nil {
 		return v
 	}
 	return nil
 }
 
-func (rc *redisCache) Lpop(key string) (string,error) {
+func (rc *Redis) Lpop(key string) (string,error) {
 	return redis.String( rc.do("LPOP", key))
 }
 
-func (rc *redisCache) Rpop(key string) (string,error) {
+func (rc *Redis) Rpop(key string) (string,error) {
 	return redis.String( rc.do("RPOP", key))
 }
 
-func (rc *redisCache) GetMulti(keys []string) []interface{} {
+func (rc *Redis) GetMulti(keys []string) []interface{} {
 	size := len(keys)
 	var rv []interface{}
 	c := rc.p.Get()
@@ -154,7 +142,7 @@ func (rc *redisCache) GetMulti(keys []string) []interface{} {
 	return rv
 }
 
-func (rc *redisCache) Put(key string, val interface{}, timeout time.Duration) error {
+func (rc *Redis) Put(key string, val interface{}, timeout time.Duration) error {
 	var err error
 	if _, err = rc.do("SETEX", key, int64(timeout / time.Second), val); err != nil {
 		return err
@@ -166,7 +154,7 @@ func (rc *redisCache) Put(key string, val interface{}, timeout time.Duration) er
 	return err
 }
 
-func (rc *redisCache) Set(key string, val interface{}) error {
+func (rc *Redis) Set(key string, val interface{}) error {
 	var err error
 	if _, err = rc.do("SET", key, val); err != nil {
 		return err
@@ -174,7 +162,7 @@ func (rc *redisCache) Set(key string, val interface{}) error {
 	return err
 }
 
-func (rc *redisCache) Rpush(key string,val interface{} ) error  {
+func (rc *Redis) Rpush(key string,val interface{} ) error  {
 	var err error
 	if _, err = rc.do("RPUSH", key, val); err != nil {
 		return err
@@ -182,7 +170,7 @@ func (rc *redisCache) Rpush(key string,val interface{} ) error  {
 	return err
 }
 
-func (rc *redisCache) Lpush(key string,val interface{} ) error  {
+func (rc *Redis) Lpush(key string,val interface{} ) error  {
 	var err error
 	if _, err = rc.do("LPUSH", key, val); err != nil {
 		return err
@@ -190,11 +178,11 @@ func (rc *redisCache) Lpush(key string,val interface{} ) error  {
 	return err
 }
 
-func (rc *redisCache) LLen(key string) (int, error)  {
+func (rc *Redis) LLen(key string) (int, error)  {
 	return redis.Int(rc.do("LLEN",key))
 }
 
-func (rc *redisCache) Delete(key string) error {
+func (rc *Redis) Delete(key string) error {
 	var err error
 	if _, err = rc.do("DEL", key); err != nil {
 		return err
@@ -203,7 +191,7 @@ func (rc *redisCache) Delete(key string) error {
 	return err
 }
 
-func (rc *redisCache) IsExist(key string) bool {
+func (rc *Redis) IsExist(key string) bool {
 	v, err := redis.Bool(rc.do("EXISTS", key))
 	if err != nil {
 		return false
@@ -216,54 +204,35 @@ func (rc *redisCache) IsExist(key string) bool {
 	return v
 }
 
-func (rc *redisCache) Incr(key string) error {
+func (rc *Redis) Incr(key string) error {
 	_, err := redis.Bool(rc.do("INCRBY", key, 1))
 	return err
 }
 
-func (rc *redisCache) Decr(key string) error {
+func (rc *Redis) Decr(key string) error {
 	_, err := redis.Bool(rc.do("INCRBY", key, -1))
 	return err
 }
 
-func (rc *redisCache)Hset(key ,member string,val interface{}) bool {
+func (rc *Redis)Hset(key ,member string,val interface{}) bool {
 	if _, err := rc.do("HSET", key, member,val); err != nil {
 		return false
 	}
 	return true
 }
 
-func (rc *redisCache)Hdel(key ,member string) bool {
+func (rc *Redis)Hdel(key ,member string) bool {
 	if _, err := rc.do("HDEL", key, member); err != nil {
 		return false
 	}
 	return true
 }
 
-func (rc *redisCache) ClearAll() error {
-	cachedKeys, err := redis.Strings(rc.do("HKEYS", rc.key))
-	if err != nil {
-		return err
-	}
-	for _, str := range cachedKeys {
-		if _, err = rc.do("DEL", str); err != nil {
-			return err
-		}
-	}
-	_, err = rc.do("DEL", rc.key)
-	return err
-}
-
-
-func (rc *redisCache) StartAndGC(config string) error {
+func (rc *Redis) NewRedis(config string) (*Redis,error) {
 	var cf map[string]string
 	json.Unmarshal([]byte(config), &cf)
-
-	if _, ok := cf["key"]; !ok {
-		cf["key"] = DefaultKey
-	}
 	if _, ok := cf["conn"]; !ok {
-		return errors.New("config has no conn key")
+		return nil,errors.New("config has no conn key")
 	}
 	if _, ok := cf["dbNum"]; !ok {
 		cf["dbNum"] = "0"
@@ -280,11 +249,10 @@ func (rc *redisCache) StartAndGC(config string) error {
 
 	c := rc.p.Get()
 	defer c.Close()
-
-	return c.Err()
+	return rc,c.Err()
 }
 
-func (rc *redisCache) connectInit() {
+func (rc *Redis) connectInit() {
 	dialFunc := func() (c redis.Conn, err error) {
 		c, err = redis.Dial("tcp", rc.conninfo)
 		if err != nil {
